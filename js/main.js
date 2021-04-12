691f37735c1c37fefaecf72f0b8eb26bb98bef12
ngf93 <nabiullinaguzel93@gmail.com>
2021-04-12 04:49:12 +0300

    in process

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/* current year */
+let now = new Date();
+let cur_year = now.getFullYear();
+console.log(cur_year);
+document.querySelector('#year').innerHTML = cur_year;
+
+
+/*************** 
+CUSTOM SELECT sort
+****************/
+let arr_selects = Array.from(document.querySelectorAll('.custom-select'));
+arr_selects.forEach(function(item, i, arr) {
+    let btn = item.querySelector('button');
+    let options = item.querySelector('.options');
+    btn.addEventListener('click', () => {
+        options.style.display = (options.style.display == 'block') ? 'none' : 'block'
+    });
+
+    let arr_radio = Array.from(item.querySelectorAll('input[type="radio"]'));
+    arr_radio.forEach(function(item, i, arr) {
+        if(item.checked){
+            btn.innerHTML = item.value;
+        }
+        item.addEventListener('change', (event) => {
+            btn.innerHTML = item.value;
+        });
+    });
+});
